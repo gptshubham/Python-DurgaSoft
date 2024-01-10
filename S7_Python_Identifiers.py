81c98dc42f9c884bbf8fe56d1307bb82a669d47d
gptshubham <shubhamca76@gmail.com>
2024-01-10 15:07:33 +0530

    L7 on identifiers completes

---
+++ S7_Python_Identifiers.py
@@ -0,0 +1,44 @@
+# _ is allowed
+first_name = "Shubham"
+print(first_name)
+
+# $ is not allowed
+# $econd_name = "kumar"
+second_Name = "Kumar"
+print(second_Name)
+
+# using integers in the beginning of identifier is not allowed
+# 123last_name = "gupta"
+last_name = "Gupta"
+print(last_name)
+
+# using _ in the begging is allowed
+# variable that starts with _ it's called protected variable (oops)
+_gender = "Male"
+print(_gender)
+
+# case sensitive
+First_Name = "Abhi"
+print(First_Name)
+print(first_name)
+firstName = "Sonal"
+print((firstName))
+
+age123 = 22
+print(age123)
+
+# using Reserved Words as Identifiers is not allowed
+"""
+# if = 20
+# print(if)
+"""
+
+# space is not allowed
+"""
+first name = "shubham"
+print(first name)
+"""
+
+# Reserved Words
+import keyword
+print(keyword.kwlist)
