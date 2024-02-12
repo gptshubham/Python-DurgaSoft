4df2bb5d5fd038faf54a2ab9d2541fe8c540ec68
gptshubham <shubhamca76@gmail.com>
2024-02-12 14:43:14 +0530

    dict methods completed

--- 1-Language-Fundamentals/12_Dictionary.py
+++ 1-Language-Fundamentals/12_Dictionary.py
@@ -147,3 +147,141 @@
 # print(d.get(7))
 # print(d.get(45))
 # print(d.get(45,'Key not Available!'))
+
+# # .pop()
+# d = {1:'India',2:'Nepal',3:'Bhutan',4:'Afghanistan',5:'Myanmar',6:'Bangladesh',7:'Sri Lanka',8:'Maldives',9:'China',10:'Pakistan'}
+# popped = d.pop(9)
+# print('Popped :',popped)
+# # popped = d.pop(100)   # KeyError: 100
+# popped = d.pop(10)
+# print('Popped :',popped)
+# print(d)
+
+# # .popitem()
+# d = {1:'India',2:'Nepal',3:'Bhutan',4:'Afghanistan',5:'Myanmar',6:'Bangladesh',7:'Sri Lanka',8:'Maldives',9:'China',10:'Pakistan'}
+# popped = d.popitem()
+# print('Popped Item :',popped)
+#
+# # d = {}
+# # popped = d.popitem()
+# # print(popped)    # KeyError: 'popitem(): dictionary is empty'
+#
+# # basic example
+# d = {}
+# i = 0
+# while i<10:
+#     d[chr(65+i)] = 100 + 10*i
+#     i += 1
+# print(d)
+#
+# while len(d) != 0:
+#     print('Processing Item:',d.popitem())
+
+# # .keys()
+# d = {10:'Sachin',7:'Dhoni',18:'Kohli',45:'Rohit'}
+# k = d.keys()
+# print(k)
+# # iteration --> dict_keys
+# for key in k:
+#     print(key,end=' ')
+# print()
+
+# # .values()
+# d = {10:'Sachin',7:'Dhoni',18:'Kohli',45:'Rohit'}
+# v = d.values()
+# print(v)
+# # iteration --> dict_values
+# for value in v:
+#     print(value,end=' ')
+# print()
+# # Duplicate Values --> allowed
+# d = {100:'Dhoni',200:'Dhoni',300:'Dhoni'}
+# print(d.values())
+
+# # .items()
+# d = {10:'Sachin',7:'Dhoni',18:'Kohli',45:'Rohit'}
+# d_elements = d.items()
+# print(d_elements)
+# # iteration --> dict_items
+# for item in d_elements:
+#     print(item)
+# # iteration and String formatting
+# for k,v in d.items():
+#     print('{}:{}'.format(k,v))
+
+# # .setdefault()
+# d = {10:'Sachin',7:'Dhoni',18:'Kohli',45:'Rohit'}
+# print(d.setdefault(63,'SKY'))
+# print(d)
+
+# # .update()
+# d1 = {10:'Sachin',7:'Dhoni'}
+# print(d1)
+# d2 = {18:'Kohli',45:'Rohit'}
+# print(d2)
+# d1.update(d2)
+# print(d1)
+
+# # aliasing vs. cloning
+# d1 = {10:'Sachin',7:'Dhoni'}
+# # aliasing
+# d2 = d1
+# # cloning
+# d3 = d1.copy()
+# print(id(d1))
+# print(id(d2))
+# print(id(d3))
+# print(d1 is d2)
+# print(d1 is d3)
+
+# # Program 1: write a program to take dictionary from
+# # keyboard and print the sum of values
+#
+# # using loops
+# d = eval(input('Enter a Dictionary : '))  # {'A':100,'B':200,'C':300,'D':400}
+# total = 0
+# for v in d.values():
+#     total += v
+# print('Sum :',total)
+#
+# # without loop
+# d1 = eval(input('Enter a Dictionary : '))
+# print('Sum :',sum(d1.values()))
+
+# # Program 2: WAP to find number of occurrences
+# # of each letter present in the given string
+#
+# # Approach 1: using if-else
+# print('Program 2: Approach 1')
+# s = 'shubhamgupta'
+# d = {}
+# for i in s:
+#     if i in d:
+#         d[i] += 1
+#     else:
+#         d[i] = 1
+# # print(d)
+# for k,v in sorted(d.items()):
+#     print('{} : {}'.format(k,v))
+#
+# # Approach 2: using .get()
+# print('Program 2: Approach 2')
+# s = 'shubhamgupta'
+# new_d = {}
+# for i in s:
+#     new_d[i] = new_d.get(i,0) + 1
+# # print(new_d)
+# for k,v in sorted(new_d.items()):
+#     print('{} : {}'.format(k,v))
+
+# # Program 3: WAP to find the number of occurrences
+# # of each vowels present in the given string
+# print('Program 3')
+# s = 'shubhamkumargupta'
+# vowels = ['a','e','i','o','u']
+# d = {}
+# for i in s:
+#     if i in vowels:
+#         d[i] = d.get(i,0) + 1
+#
+# print(d)
