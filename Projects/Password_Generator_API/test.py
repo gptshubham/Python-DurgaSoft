ad069a30e2498215630033532c1a631d55371186
gptshubham <shubhamca76@gmail.com>
2024-02-23 17:47:17 +0530

    password generator api mini project completed

---
+++ Projects/Password_Generator_API/test.py
@@ -0,0 +1,22 @@
+# import password
+#
+# pwd = password.password(10)
+# print(pwd)
+#
+# pwd = password.password(7)
+# print(pwd)
+#
+# pwd_len = int(input('Enter the length of password : '))
+# pwd = password.password(pwd_len)
+# print(pwd)
+
+# from password import password
+# pwd = password(10)
+# print(pwd)
+#
+# pwd = password(7)
+# print(pwd)
+#
+# pwd_len = int(input('Enter the length of password : '))
+# pwd = password(pwd_len)
+# print(pwd)
