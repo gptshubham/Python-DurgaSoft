ce9094b9147427f9bfe9024772909849082e804e
gptshubham <shubhamca76@gmail.com>
2024-05-09 13:08:49 +0530

    python revision : basic data types completed

---
+++ Revision-Round-1/ch1_Language_Fundamentals_Revision.py
@@ -0,0 +1,77 @@
+# print('Hello world')
+
+# Identifiers
+'''
+firstName = 'Shubham'
+# print(firstname)
+# NameError: name 'firstname' is not defined. Did you mean: 'firstName'?
+print(firstName)
+'''
+
+# Reserved Words
+'''
+# pritn(firstName)
+# NameError: name 'pritn' is not defined. Did you mean: 'print'?
+
+import keyword
+print(keyword.kwlist)
+'''
+
+# Python Data Types
+'''
+# basic data types --> int,str,float,bool,complex
+
+# int data type
+a = 10
+print(a)
+print(type(a))
+print(id(a))
+
+a = 0b1111
+print(a)
+a = 0o123
+print(bin(15))
+print(a)
+print(oct(83))
+a = 0xFACE
+print(a)
+print(hex(64206))
+
+# float data type
+f = 1.234
+print(f)
+print(type(f))
+print(id(f))
+# Exponential Form
+f = 1.2e3
+print(f)
+f = 1.2E3
+print(f)
+f = 1.2e5
+print(f)
+
+# complex data type --> skipped
+
+# bool data type
+print(type(True))
+print(type(False))
+print(True + True)
+print(False + True)
+'''
+# string data type
+# # multi line string
+# s = '''shubham
+# kumar
+# gupta'''
+# print(s)
+# s = """shubham
+# kumar
+# gupta"""
+# print(s)
+#
+# print('Python Classes by "Durga Sir" are awesome')
+# print("Python Classes by 'Durga Sir' are awesome")
+# print("""Durga Sir's "Python Classes" are awesome""")
+
+# Type Casting / Type Conversion
+# skipped as we just covered this topic as part of our Week 1 curriculum at IIT Madras
