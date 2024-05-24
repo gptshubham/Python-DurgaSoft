c61ee7e0fc67bd9f79698370d0073095bb62eaac
gptshubham <shubhamca76@gmail.com>
2024-05-24 18:14:42 +0530

    decorators practical examples part 1 completed

--- 3-Decorators-&-Generators/Python_Decorators.py
+++ 3-Decorators-&-Generators/Python_Decorators.py
@@ -28,6 +28,7 @@ new_calc(20,4)
 '''
 
 # Nested Functions
+'''
 def outer():
     print('outer function started')
     def inner():
@@ -49,8 +50,10 @@ print()
 
 # inner()
 # NameError: name 'inner' is not defined
+'''
 
 # outer function can return inner function --> Higher Order Function
+'''
 def outer():
     print('outer function started')
     def inner():
@@ -69,7 +72,7 @@ newf()
 newf()
 print()
 
-# Tricky Doubt - what if outer() returns inner() instead of return
+# Tricky Doubt - what if outer() returns inner() instead of inner
 def outer():
     print('outer function started')
     def inner():
@@ -95,5 +98,95 @@ def fy():
 f1(fx) # we are calling f1() by passing fx function as argument
 print()
 f1(fy)
+'''
 
-# Decorator
+# Decorator Function
+
+# Theoretical Example
+'''
+def decor(func):
+    def inner():
+        print('send person to Beauty Parlour')
+        print('showing a person with full decoration')
+    return inner
+
+# function without decorator
+def display():
+    print('Showing a person as it is')
+
+display()        # display() function is executed
+print()
+
+# function with decorator
+@decor
+def display():
+    print('Showing a person as it is')
+
+display()         # inner() function is executed
+print()
+display()         # inner() function is executed
+print()
+display()         # inner() function is executed
+print()
+display()         # inner() function is executed
+'''
+
+
+# Basic Practical Example of Decorator
+'''
+def decor_for_add(func):
+    def inner(x, y):      # No. of arguments must match with original functon add
+        print('#' * 40)
+        print(f'The sum of {x} and {y} is ', end='')
+        func(x, y)        # Calling original function add
+        print('#' * 40)
+    return inner
+
+# add function without decorator
+def add(a, b):
+    print(a + b)
+
+
+add(10,20)
+print()
+
+# add function with decorator
+@decor_for_add
+def add(a, b):
+    print(a + b)
+
+add(10,20)
+add(100,200)
+add(1000,2000)
+add(777,888)
+'''
+
+# Decorator Example : Spacial functionality for specific Entity
+def decor_for_dhoni(func):
+    def inner(name):
+        names = ['Dhoni', 'Kohli', 'Rohit', 'Sachin']
+        if name in names:
+            print('#' * 40)
+            print(f'Hello {name}!')
+            print('I am your Biggest Fan!')
+            print(f'Love You {name}.')
+            print('#' * 40)
+        else:
+            func(name)
+    return inner
+
+
+@decor_for_dhoni
+def wish(name):
+    print(f'Hello {name}!')
+
+
+wish('python')
+wish('Dhoni')
+wish('Jaddu')
+wish('Chahal')
+wish('Pandya')
+wish('Kohli')
+wish('Rohit')
+
+
