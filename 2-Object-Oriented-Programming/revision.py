acb079afff0910ccc27715a02b9dfeca2a302592
gptshubham <shubhamca76@gmail.com>
2024-09-05 08:57:51 +0530

    revision of oops started

--- 2-Object-Oriented-Programming/revision.py
+++ 2-Object-Oriented-Programming/revision.py
@@ -1,69 +1,36 @@
+# Session 74 : Class, Object and Reference Variable
 '''
-# let's recreate the Student class from scratch
+# Student Class Example
+class Student:
+    """This class is developed by Shubham for Demo Purpose"""
+    # variable --> attribute/ property
+    # method --> behaviour/ actions
+
+print(Student.__doc__)
+help(Student)
+'''
+
+# Student Class (not a full-fledged example)
+'''
 class Student:
     def __init__(self):
         print('Constructor Execution...')
-        self.name='shubham'
-        self.rollno=101
-        self.marks=98
+        self.name = 'Shubham'
+        self.roll_no = 91
+        self.marks = 100
     def talk(self):
-        print('Hello I am',self.name)
-        print('My Roll No. is',self.rollno)
-        print('My Marks is',self.marks)
+        print(f'Hello I am: {self.name}')
+        print(f'My Roll Number is: {self.roll_no}')
+        print(f'My Marks are: {self.marks}')
 
-s = Student()
-print(s.name)
-print(s.rollno)
-print(s.marks)
-s.talk()
 
 s1 = Student()
+print(s1.name, s1.roll_no, s1.marks)
+s1.talk()
+# print(s.__doc__)
+# help(Student)
 s2 = Student()
-
+print(s1.name, s1.roll_no, s1.marks)
 print(id(s1))
 print(id(s2))
-print(s1 is s2)
 '''
-
-# Instance Variables
-'''
-class Test:
-    def __init__(self):
-        self.a = 10
-
-    def m1(self):
-        self.a = 20
-        self.b = 30
-
-    def m2(self):
-        self.e = 60
-        self.f = 70
-
-t = Test()
-print(t.__dict__)
-t.m1()
-print(t.__dict__)
-t.a = 40
-t.c = 50
-print(t.__dict__)
-# accessing instance variables
-print(t.a,t.b,t.c)
-'''
-
-# Destructor
-import time
-class Test:
-    def __init__(self):
-        print('Object initialization...')
-
-    def __del__(self):
-        print('Resource deallocation and cleanup activities...')
-
-t1 = Test()
-t1=None
-t2 = Test()
-del t2
-t3 = Test()
-time.sleep(5)
-print('End of Application')
-
